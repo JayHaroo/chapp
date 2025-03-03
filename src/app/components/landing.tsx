3652746cba17d4daea851be322ea2cf49bdbc0d2
M-Jey <mjeyict3@gmail.com>
2025-03-03 19:11:30 +0800

    Refactor Landing component to use new Google Fonts and update text styles

--- src/app/components/landing.tsx
+++ src/app/components/landing.tsx
@@ -1,12 +1,21 @@
 import { IoArrowForwardCircle } from "react-icons/io5";
+import { JetBrains_Mono, Raleway } from "next/font/google";
+
+const raleway = Raleway({
+  subsets: ["latin"]
+});
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"]
+});
 
 export default function Landing() {
   return (
     <>
       <div className="bg-[url(/img/bg.jpg)] bg-cover bg-center h-screen w-screen">
         <div className="w-[450px] h-lvh bg-white rounded-md text-black text-center justify-items-center content-center ">
-          <div className="text-[2rem] font-bold">Geoined</div>
-          <div className="font-light">Save your rights to a blockchain</div>
+          <div className={`text-[3rem] font-bold ${raleway.className}`}>Geoined</div>
+          <div className={`font-light ${jetbrainsMono.className}`}>Save your rights to a blockchain</div>
           <IoArrowForwardCircle size={60} className="cursor-pointer" />
         </div>
       </div>
