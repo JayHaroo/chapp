13bafa7cbf76b2cc8b401e2a744b80e2c562c0d6
M-Jey <mjeyict3@gmail.com>
2025-03-03 22:02:42 +0800

    Add RainbowKit integration and update layout structure

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,7 +1,11 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit"
+
 export default function Form() {
     return (
         <>
-        <div className="">Hello</div>
+        <div className="flex justify-center items-center h-[100vh]">
+            <ConnectButton />
+        </div>
         </>
     )
 };
