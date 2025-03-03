0a5b22e59c722ba6faccab91133ef6555c604a82
M-Jey <mjeyict3@gmail.com>
2025-03-03 16:21:54 +0800

    Update background image from PNG to JPG in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="bg-[url(/img/bg.png)] bg-cover bg-center h-screen w-screen">
+      <div className="bg-[url(/img/bg.jpg)] bg-cover bg-center h-screen w-screen">
         <div className="w-[450px] h-lvh bg-white rounded-md"></div>
       </div>
     </>
