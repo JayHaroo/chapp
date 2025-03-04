06cb6b4a9b7ed0c67ce131b605c11002114e1e21
M-Jey <mjeyict3@gmail.com>
2025-03-04 08:02:41 +0800

    Add dotenv and web3 dependencies; update Form component layout with navigation

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import { ConnectButton } from "@rainbow-me/rainbowkit"
+import { IoIosArrowBack } from "react-icons/io";
+import Link from "next/link";
 
 export default function Form() {
     return (
         <>
-        <div className="flex justify-center items-center h-[100vh]">
-            <ConnectButton />
+        <div className="flex justify-center items-center mt-[2rem]">
+            <div className="flex justify-evenly items-center w-[100vh]">
+                <ConnectButton />
+                <Link href="/">
+                    <IoIosArrowBack size={30} color="white" />
+                </Link>
+            </div>
+            
         </div>
         </>
     )
