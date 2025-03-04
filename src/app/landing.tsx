c70e39bc1c4f9141b8795f82d6a20acddf96284d
M-Jey <mjeyict3@gmail.com>
2025-03-04 20:44:13 +0800

    Enhance Landing component layout for improved responsiveness on smaller screens

--- src/app/landing.tsx
+++ src/app/landing.tsx
@@ -14,16 +14,15 @@ const jetbrainsMono = JetBrains_Mono({
 
 export default function Landing() {
   return (
-    <>
-      <div className="bg-[url(/img/bg.jpg)] bg-cover bg-center h-screen w-screen">
-        <div className="w-[450px] h-lvh bg-white rounded-md text-black text-center justify-items-center content-center ">
-          <div className={`text-[3rem] font-bold ${raleway.className}`}>Chapp!</div>
-          <div className={`font-light ${jetbrainsMono.className}`}>Let`s talk unbounded.</div>
-          <Link href="/form">
-            <IoArrowForwardCircle size={60} className="cursor-pointer" title="Proceed"/>
-          </Link>
-        </div>
+    <div className="bg-[url(/img/bg.jpg)] bg-cover bg-center h-screen w-screen max-sm:justify-items-center max-sm:content-center">
+      <div className="w-[450px] h-lvh bg-white rounded-md text-black text-center flex flex-col items-center justify-center max-sm:w-[35vh] max-sm:h-[60vh] max-sm:rounded-3xl">
+        <div className="text-[3rem] font-bold">Chapp!</div>
+        <div className="font-light">Let’s talk unbounded.</div>
+        <Link href="/form">
+          <IoArrowForwardCircle size={60} className="cursor-pointer" title="Proceed"/>
+        </Link>
       </div>
-    </>
+    </div>
   );
 }
+
