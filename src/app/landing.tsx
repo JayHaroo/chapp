8e7b2cd449d59d80eae91752dc587d65144ef6b8
M-Jey <mjeyict3@gmail.com>
2025-03-05 13:23:40 +0800

    Add custom fonts to text elements in Landing component for enhanced typography

--- src/app/landing.tsx
+++ src/app/landing.tsx
@@ -17,8 +17,8 @@ export default function Landing() {
   return (
     <div className="bg-[url(/img/bg.png)] bg-cover bg-center h-screen w-screen max-sm:justify-items-center max-sm:content-center">
       <div className="w-[450px] h-lvh bg-white rounded-md text-black text-center flex flex-col items-center justify-center max-sm:w-[35vh] max-sm:h-[60vh] max-sm:rounded-3xl">
-        <div className="text-[3rem] font-bold">Chapp!</div>
-        <div className="font-light">Let’s talk unbounded.</div>
+        <div className={`text-[3rem] font-bold ${raleway.className}`}>Chapp!</div>
+        <div className={`font-light ${jetbrainsMono.className}`}>Let’s talk unbounded.</div>
         <Link href="/form">
           <IoArrowForwardCircle size={60} className="cursor-pointer" title="Proceed"/>
         </Link>
